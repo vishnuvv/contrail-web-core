2838729d6cd1c5bc1d7e96ea2dd476b867ecc1be
Vishnu Vardhan V <vishnuvv@juniper.net>
2014-08-28 11:06:02 +0530

    Initial checkin for the cross filter in the chart settings

--- webroot/js/web-utils.js
+++ webroot/js/web-utils.js
@@ -2496,7 +2496,7 @@ function ManageCrossFilters(){
         }
         return null;
     }
-    
+
     this.getDimension = function(cfName,dimensionName){
         var cfObj = globalObj['crossFilters'][cfName];
         if(cfObj != null && cfObj.dimensions != null && cfObj.dimensions[dimensionName] != null){
