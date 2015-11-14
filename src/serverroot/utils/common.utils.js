c14a7ce7c8fc820ce40ee925fdb4cdacff60a069
Naga Kiran <nagakiran@juniper.net>
2015-11-14 19:06:01 +0530

    Changes to display console messages in color
    Change-Id: I2c420ba88c9b04dc3cd617e843e48ec940c32d26

--- src/serverroot/utils/common.utils.js
+++ src/serverroot/utils/common.utils.js
@@ -32,6 +32,21 @@ var commonUtils = module.exports,
     _ = require('underscore'),
     redisUtils = require('./redis.utils');
 
+exports.FgGreen = "\x1b[32m%s\x1b[0m";
+exports.FgYellow = "\x1b[33m%s\x1b[0m",
+exports.FgBlue = "\x1b[34m%s\x1b[0m",
+exports.FgMagenta = "\x1b[35m%s\x1b[0m",
+exports.FgCyan = "\x1b[36m%s\x1b[0m",
+exports.FgWhite = "\x1b[37m%s\x1b[0m",
+exports.BgBlack = "\x1b[40m%s\x1b[0m",
+exports.BgRed = "\x1b[41m%s\x1b[0m",
+exports.BgGreen = "\x1b[42m%s\x1b[0m",
+exports.BgYellow = "\x1b[43m%s\x1b[0m",
+exports.BgBlue = "\x1b[44m%s\x1b[0m",
+exports.BgMagenta = "\x1b[45m%s\x1b[0m",
+exports.BgCyan = "\x1b[46m%s\x1b[0m",
+exports.BgWhite = "\x1b[47m%s\x1b[0m"
+
 if (!module.parent) {
     logutils.logger.warn(util.format(
                          messages.warn.invalid_mod_call, module.filename));
