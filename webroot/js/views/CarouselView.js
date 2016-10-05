90272718c13270841b68d7863d76a9d05de9cc57
Vishnu Vardhan V <vishnuvv@juniper.net>
2016-10-05 10:51:28 +0530

    add new carosuel and stackedarea views
    Change-Id: I0882237684ff1f5376a91bebb188c8f685c6222e

---
+++ webroot/js/views/CarouselView.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+ */
+define(['underscore', 'contrail-view'], function(_, ContrailView){
+    var CarouselView = ContrailView.extend({
+        render: function(){
+            var self = this,
+                viewConfig = self.attributes.viewConfig,
+                carouselTemplate = contrail.getTemplate4Id("carousal-view-template");
+
+            self.viewList = viewConfig.pages;
+            self.viewPlaceHolder = {};
+            self.prevIndex = 0;
+            self.currIndex = 0;
+            self.lastIndex = viewConfig.pages.length;
+
+            self.$el.html(carouselTemplate);
+            self.viewPlaceHolder = self.$el.find(".carousel-content");
+            activateView(self, self.currIndex, false);
+        },
+        next: function(){
+            var self = this;
+
+            self.currIndex = self.currIndex + 1;
+            if(self.currIndex >= self.lastIndex){
+                self.currIndex = 0;
+            }
+            if(self.prevIndex != self.currIndex){
+                self.prevIndex = self.currIndex
+                activateView(self, self.currIndex, true, 'next');
+            }
+        },
+        prev: function(){
+            var self = this;
+
+            self.currIndex = self.currIndex - 1;
+            if(self.currIndex < 0){
+                self.currIndex = self.lastIndex - 1;
+            }
+            if(self.prevIndex != self.currIndex){
+                self.prevIndex = self.currIndex
+                activateView(self, self.currIndex, true, 'prev');
+            }
+        },
+        events: {
+            'click .custom-carousel-control-left': 'prev',
+            'click .custom-carousel-control-right': 'next'
+        }
+    });
+
+    function activateView(self, index, doAnimate, direction){
+        var height = $('div.carousel').height(),
+            page = self.viewList[index].page,
+            model = ifNull(self.viewList[index].model, '');
+
+        if(!doAnimate){
+            self.renderView4Config(self.viewPlaceHolder,  model, page);
+        }else{
+            var slideDirection = ['right','left'],
+                height = $('div.carousel').height();
+
+            if(direction === 'prev')
+                slideDirection = ['left','right'];
+
+            $("div.carousel-content").hide("slide", { direction: slideDirection[0] }, 300, function() {
+                $('div.carousel').height(height);
+                $(".carousel-content").empty();
+                $("div.carousel-content").show("slide", { direction: slideDirection[1] }, 300, function(){
+                   self.renderView4Config(self.viewPlaceHolder,  model, page, null, null, null, function(){
+                       //setTimeout(function(){
+                           //$('div.carousel').height($("div.carousel-inner").children().height() * 2);
+                       //}, 1000);
+                   });
+                });
+            });
+        }
+    }
+
+    return CarouselView;
+});
