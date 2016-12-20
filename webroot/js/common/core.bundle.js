e91ac0ea272683a207d6caee11506b2eb1015972
Vishnu Vardhan V <vishnuvv@juniper.net>
2016-12-20 14:10:53 +0530

    dashboard carousel changes
    Change-Id: Iff7934660e99c2a30ffe1f90f95ff568ba181a27

--- webroot/js/common/core.bundle.js
+++ webroot/js/common/core.bundle.js
@@ -51,7 +51,7 @@ define([
     'mon-infra-alert-grid-view',
     'mon-infra-log-list-view',
     'mon-infra-sysinfo-view',
-    'mon-infra-dashboard-view'
+    //'mon-infra-dashboard-view'
 
 ], function (CoreHandlebarsUtils, CoreUtils, CoreHashUtils, CoreConstants, CoreFormatters, Cache, CoreLabels,
              CoreMessages, CoreViewsDefaultConfig, Contrail, CoreContrailFormElements, ChartUtils, CoreCommonTmpls) {
