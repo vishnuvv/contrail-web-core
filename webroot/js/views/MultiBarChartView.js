8f51a50ace9f512d88b1be53a2eaf28fd9cf8214
Vishnu <vishnuvv@juniper.net>
2017-10-17 17:54:53 +0530

    horizontal multi bar chart and gristack widgt title support.
    Change-Id: Iee587cf66ba618eacb54dabdbbb3372c7c26a1b5

--- webroot/js/views/MultiBarChartView.js
+++ webroot/js/views/MultiBarChartView.js
@@ -104,8 +104,9 @@ define([
     function getChartViewConfig(chartData, chartOptions) {
         var chartViewConfig = {};
         var chartDefaultOptions = {
-            margin: {top: 10, right: 30, bottom: 20, left: 60},
+            margin: {top: 10, right: 30, bottom: 50, left: 150},
             height: 250,
+            barOrientation: 'vertical',
             xAxisLabel: 'Items',
             xAxisTickPadding: 10,
             yAxisLabel: 'Values',
